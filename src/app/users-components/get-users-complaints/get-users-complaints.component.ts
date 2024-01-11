831eea4e880384300b388749896782be85508a48
Umairmaqsood <umairmaqsood089@gmail.com>
2024-01-11 22:29:50 +0500

    enable sortings

--- src/app/users-components/get-users-complaints/get-users-complaints.component.ts
+++ src/app/users-components/get-users-complaints/get-users-complaints.component.ts
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material/src/public-api';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-get-users-complaints',
@@ -17,7 +18,12 @@ import { MatPaginator } from '@angular/material/paginator';
         <h1 style="font-weight:bold">Complaints History</h1>
 
         <div>
-          <table mat-table [dataSource]="dataSource" class="mat-elevation-z8">
+          <table
+            mat-table
+            [dataSource]="dataSource"
+            matSort
+            class="mat-elevation-z8"
+          >
             <ng-container matColumnDef="createdAt">
               <th
                 mat-header-cell
@@ -88,10 +94,11 @@ import { MatPaginator } from '@angular/material/paginator';
           </table>
 
           <mat-paginator
-            #paginator
-            [pageSize]="10"
-            [pageSizeOptions]="[5, 10, 20, 100]"
-            showFirstLastButtons
+            [length]="resultsLength"
+            [pageIndex]="pageIndex"
+            [pageSize]="pageSize"
+            [pageSizeOptions]="pageSizeOptions"
+            (page)="onPageChange($event)"
           ></mat-paginator>
         </div>
       </ng-container>
@@ -146,17 +153,17 @@ import { MatPaginator } from '@angular/material/paginator';
 export class GetUsersComplaintsComponent {
   isAsyncCall = false;
   userId: any;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  resultsLength = 0;
+  pageIndex = 0;
+  pageSize = 10;
+  pageSizeOptions: number[] = [2, 5, 10, 20, 100];
 
   displayedColumns: string[] = ['createdAt', 'title', 'details', 'isFixed'];
 
   dataSource = new MatTableDataSource([]);
-
-  @ViewChild(MatPaginator, { static: false })
-  set paginator(value: MatPaginator) {
-    if (this.dataSource) {
-      this.dataSource.paginator = value;
-    }
-  }
 
   constructor(
     private snackBar: MatSnackBar,
@@ -176,9 +183,6 @@ export class GetUsersComplaintsComponent {
     }
   }
 
-  page = 1;
-  pageSize = 10;
-
   getUsersComplaints() {
     this.isAsyncCall = true;
     this.authService.getUsersComplaints(this.userId).subscribe(
@@ -186,7 +190,13 @@ export class GetUsersComplaintsComponent {
         if (res) {
           const data = res?.results;
           this.dataSource = new MatTableDataSource(data);
+          this.resultsLength = res?.results?.totalCount;
           this.isAsyncCall = false;
+
+          // Reset paginator after filtering
+          if (this.paginator) {
+            this.paginator.firstPage();
+          }
         }
       },
       (error) => {
@@ -195,6 +205,13 @@ export class GetUsersComplaintsComponent {
         this.error();
       }
     );
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.sort.sortChange.emit(this.sort);
+    this.getUsersComplaints();
   }
 
   error(): void {
